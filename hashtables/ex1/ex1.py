ff1a4bf2b30151587738a69276e35fc3e1ba9fe2
Kerri-Ann <kaybee1jam@yahoo.com>
2020-04-10 12:34:05 -0400

    completed mvp

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,19 +1,35 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
-from hashtables import (HashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+from hashtables import (
+    HashTable,
+    hash_table_insert,
+    hash_table_remove,
+    hash_table_retrieve,
+    hash_table_resize,
+)
 
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
     """
-    YOUR CODE HERE
-    """
+	YOUR CODE HERE
+	"""
+    # insert weights in ht (weight, index)
+    for i in range(0, len(weights)):
+        hash_table_insert(ht, weights[i], i)
 
-    return None
+    # Check if ht contains a key for limit - weight
+    found = []
+    # Loop thru weights to find key
+    for i in range(len(weights) - 1, -1, -1):
+        find_key = limit - weights[i]
+        if hash_table_retrieve(ht, find_key):
+            found.append(i)
+
+    if len(found) > 0:
+        return found
+    else:
+        return None
 
 
 def print_answer(answer):
